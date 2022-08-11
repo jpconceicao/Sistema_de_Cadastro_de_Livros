5a71914309f0dbb40b444957a19ac321f23cd77f
Jorge Palma <jorgepalmaconceicao@gmail.com>
2022-08-10 23:43:17 -0300

    Criando repositório

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+def start():
+    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    start()
+
+
